8e39a02ca8c6975525b02b0a08fb0e997576175c
Dilip <dstha0304@gmail.com>
2026-07-16 17:14:53 +0545

    Implementation of Queue for Bank Token System

---
+++ src/queue/MyQueue.java
@@ -0,0 +1,47 @@
+package queue;
+
+public class MyQueue<T> {
+    Node<T> front;
+    Node<T> rear;
+
+    public void enqueue(T data)
+    {
+        Node<T> n = new Node<>(data);
+        if(rear == null)
+        {
+            rear = front = n;
+            return;
+        }
+        rear.next=n;
+        rear=n;
+    }
+
+    public T dequeue()
+    {
+        if(front == null)
+        {
+            return null;
+        }
+        T data = front.data;
+        front=front.next;
+        if(front == null)
+        {
+            rear=null;
+        }
+        return data;
+    }
+    public String display()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<html>");
+        sb.append("<h1>Waiting List</h1>");
+        Node<T> temp = front;
+        while(temp!=null)
+        {
+            sb.append(temp.data.toString());
+            temp = temp.next;
+        }
+        sb.append("<html");
+        return sb.toString();
+    }
+}
